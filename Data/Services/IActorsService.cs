53f6037c71dd6f85b3dfb1ccaf94c1e05f6f5118
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-06-02 02:22:03 +0500

    Added Interface for Actor Service

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using eTicketing.Models;
+
+namespace eTicketing.Data.Services
+{
+	public interface IActorsService
+	{
+		IEnumerable<Actor> GetAll();
+
+		Actor GetById(int id);
+
+		void Add(Actor actor);
+
+		Actor Update(int id, Actor newActor);
+
+		void Delete(int id);
+	}
+}
