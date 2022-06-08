64ffc27a033a340d1d8db9ccf67c14f08d6fdbf6
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-06-09 01:29:39 +0500

    Method for adding item into Shopping Cart

--- Data/Cart/ShoppingCart.cs
+++ Data/Cart/ShoppingCart.cs
@@ -17,6 +17,27 @@ namespace eTicketing.Data.Cart
 			_context = context;
 		}
 
+		public void AddItemToCart(Movie movie)
+        {
+			var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartid);
+
+			if(shoppingCartItem==null)
+            {
+				shoppingCartItem = new ShoppingCartItem()
+				{
+					ShoppingCartId = ShoppingCartid,
+					Movie = movie,
+					Amount = 1,
+				};
+				_context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+			else
+            {
+				shoppingCartItem.Amount++;
+            }
+			_context.SaveChanges();
+        }
+
 		public List<ShoppingCartItem> GetShoppingCartItems()
         {
 			return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartid).Include(n => n.Movie).ToList());
