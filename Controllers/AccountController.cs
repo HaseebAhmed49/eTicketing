ba40e0993c00e81c5b8e8bd99f26131dbc1e6a26
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-06-13 00:03:08 +0500

    Added Registration Code

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using eTicketing.Data;
+using eTicketing.Data.Static;
 using eTicketing.Data.ViewModels;
 using eTicketing.Models;
 using Microsoft.AspNetCore.Identity;
@@ -53,6 +54,32 @@ namespace eTicketing.Controllers
             TempData["Error"] = "Wrong Credentials. Please, try again!";
             return View(loginVM);
 
+        }
+
+        public IActionResult Register() => View(new RegisterVM());
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if(user!=null)
+            {
+                TempData["Error"] = "This Email Address is already in use";
+                return View(registerVM);
+            }
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser,registerVM.Password);
+            if(newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser,UserRoles.User);
+
+            return View("RegisterCompleted");
         }
     }
 }
